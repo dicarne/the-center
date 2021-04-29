f714439e4e360c5397e89ded0b886ee26e1d938a
dicarne <hzdingjiahe@outlook.com>
2021-04-29 15:32:51 +0800

    close https://gitee.com/dicarne/the-center/issues/I3OTRO

---
+++ TheCenterServer/TheCenterServer/Hub/WorkspaceHub.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace TheCenterServer
+{
+    public class WorkspaceHub: Hub
+    {
+        public async Task SendMessageToBoard(string workspace, string boardID, string data)
+        {
+            await Clients.All.SendAsync("ReceiveBoard", workspace, boardID, data);
+        }
+
+        public async override Task OnConnectedAsync()
+        {
+            Console.WriteLine(Context.ConnectionId);
+        }
+    }
+}
---
+++ TheCenterServer/TheCenterServer/Startup.cs
@@ -0,0 +1,66 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.AspNetCore.ResponseCompression;
+
+namespace TheCenterServer
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "TheCenterServer", Version = "v1" });
+            });
+            services.AddSignalR();
+            services.AddResponseCompression(opts =>
+            {
+                opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(new[] { "application/octet-stream" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseResponseCompression();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TheCenterServer v1"));
+            }
+            
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapHub<WorkspaceHub>("/workspace");
+            });
+
+        }
+    }
+}
