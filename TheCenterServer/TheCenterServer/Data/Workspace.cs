853dfb9940cc052d74d1dfc06a0c8b2d66c4d1ff
dicarne <hzdingjiahe@outlook.com>
2021-04-30 01:07:09 +0800

    增加服务端UI框架代码。

--- TheCenterServer/TheCenterServer/Data/Workspace.cs
+++ TheCenterServer/TheCenterServer/Data/Workspace.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections.Generic;
 
-namespace TheCenterServer{
-    public class Workspace{
-        public string WName{get;set;}
+namespace TheCenterServer
+{
+    public class Workspace
+    {
+        public string WName { get; set; }
         public List<Board> Boards { get; set; }
     }
 
@@ -16,4 +18,15 @@ namespace TheCenterServer{
         public int W { get; set; } = 12;
         public int H { get; set; } = 1;
     }
+
+    public class UICom
+    {
+        public string Id { get; set; }
+        public string type { get; set; }
+        public int W { get; set; }
+        public int H { get; set; }
+        public Dictionary<string, string> Prop { get; set; }
+        public List<string> Event { get; set; }
+    }
+
 }
