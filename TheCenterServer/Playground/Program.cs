660152e7238ae479c16bcdfbb55fc29aecf5fb72
18758076416 <louhanxin@akey.me>
2021-05-07 19:07:16 +0800

    修改依赖

---
+++ TheCenterServer/Playground/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.Json;
+namespace Playground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(JsonSerializer.Serialize(new Test() { A = 100 }));
+        }
+
+        class Test
+        {
+
+            public int A { get; set; }
+            public string _B { get; set; }
+        }
+    }
+}
