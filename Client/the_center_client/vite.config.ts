a4a0e862aea859c7b4c9d4bb41d3b19f68cc721c
dicarne <hzdingjiahe@outlook.com>
2021-05-12 00:22:20 +0800

    配置vite发布地址，无需手动转移文件了。

--- Client/the_center_client/vite.config.ts
+++ Client/the_center_client/vite.config.ts
@@ -4,5 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: './'
+  base: './',
+  build: {
+    outDir: "../the_center_desktop/web"
+  }
+
 })
