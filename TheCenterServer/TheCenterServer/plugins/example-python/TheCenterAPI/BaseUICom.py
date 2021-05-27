a75114aac529862bde0f15eb525da41adcd7545e
dicarne <hzdingjiahe@outlook.com>
2021-05-27 14:00:56 +0800

    补全python基础UI组件

--- TheCenterServer/TheCenterServer/plugins/example-python/TheCenterAPI/BaseUICom.py
+++ TheCenterServer/TheCenterServer/plugins/example-python/TheCenterAPI/BaseUICom.py
@@ -1,5 +1,7 @@
 from typing import Any, Dict, List, Optional
 from pydantic import BaseModel
+import json
+
 
 class UIEventReqBody(BaseModel):
     control: str
@@ -52,3 +54,96 @@ class Button(UIBase):
         self.bindEvent("onClick", func)
         return self
 
+class CheckBox(UIBase):
+    def __init__(self, id) -> None:
+        super().__init__("checkBox", id)
+
+    def value(self, v: bool):
+        self.ui.prop["value"] = json.dumps(v)
+        return self
+
+    def onChange(self, onchange: str):
+        self.bindEvent("onChange", onchange)
+        return self
+
+class Group(UIBase):
+    def __init__(self, id) -> None:
+        super().__init__("group", id)
+    
+    def children(self, childs: List[UICom]):
+        self.ui.prop["children"] = json.dumps(childs)
+        return self
+    
+    def horizon(self, hor: bool):
+        self.ui.prop["hor"] = json.dumps(hor)
+        return self
+
+    def spacing(self, hor_value: float, ver_value: Optional[float]):
+        s = [hor_value]
+        if(ver_value is not None):
+            s.append(ver_value)
+        self.ui.prop["spacing"] = json.dumps(s)
+
+class Input(UIBase):
+    def __init__(self, id) -> None:
+        super().__init__("input", id)
+    
+    def text(self, content):
+        self.ui.prop['text'] = content
+        return self
+
+    def onChange(self, onchange: str):
+        self.bindEvent("onChange", onchange)
+        return self
+
+    def placeholder(self, content):
+        self.ui.prop['placeholder'] = content
+        return self
+
+class More(UIBase):
+    def __init__(self, id) -> None:
+        super().__init__("more", id)
+
+    def text(self, content):
+        self.ui.prop['text'] = content
+        return self
+
+    def isshow(self, newtext: bool):
+        self.ui.prop["isshow"] = json.dumps(newtext)
+        return self
+ 
+    def onClick(self, func):
+        self.bindEvent("onClick", func)
+        return self
+
+class Transfer(UIBase):
+    def __init__(self, id) -> None:
+        super().__init__("transfer", id)
+
+    def onChange(self, onchange: str):
+        self.bindEvent("onChange", onchange)
+        return self
+
+    def text(self, content):
+        self.ui.prop['text'] = content
+        return self
+ 
+    class ShowData:
+        ava: List[str]
+        all: List[str]
+
+    def onShow(self, onShow: str):
+        self.bindEvent("onShow", onShow)
+        return self
+        
+    def avaliable(self, newt: List[str]):
+        self.ui.prop["avaliable"] = json.dumps(newt)
+        return self
+
+    def all(self, newt: List[str]):
+        self.ui.prop["all"] = json.dumps(newt)
+        return self
+
+    def type(self, newt: str):
+        self.ui.prop["type"] = newt
+        return self
--- TheCenterServer/TheCenterServer/plugins/example-python/index.py
+++ TheCenterServer/TheCenterServer/plugins/example-python/index.py
@@ -1,5 +1,5 @@
 from TheCenterAPI.APIServer import PModule, Run, reg_module
-from TheCenterAPI.BaseUICom import Button, Text
+from TheCenterAPI.BaseUICom import Button, Input, Text
 
 if __name__ == "__main__":
     Run()
@@ -7,12 +7,13 @@ if __name__ == "__main__":
 
 class HelloPythonModule(PModule):
     hello = Text("textUI", "Hello Python!")
-
     clickMe = Button("clickMe").text("点我Hello").onClick("OnClick")
+    input1 = Input("input1").placeholder("haha").onChange("OnInput1Change")
 
     def buildInterface(self):
         self.hello.text(str(self.getValue("count")))
-        return [self.hello.ui, self.clickMe.ui]
+        self.input1.text(self.getValue("inputtext"))
+        return [self.hello.ui, self.clickMe.ui, self.input1.ui]
 
     def OnClick(self):
         print("Click!")
@@ -22,8 +23,11 @@ class HelloPythonModule(PModule):
 
     def customEvent(self, control, eventname, args):
         print(control)
+
+    def OnInput1Change(self, newtext):
+        self.setValue("inputtext", newtext)
     
     def onLoad(self):
-        self.ensureValue([["count", 0]])
+        self.ensureValue([["count", 0], ["inputtext", ""]])
 
 reg_module("#", HelloPythonModule)
